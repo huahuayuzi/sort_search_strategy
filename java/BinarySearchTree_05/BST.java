77c5206d2d60e8113c09b3a2e04286ecdebdcf10
huahuayuzi <53569858+huahuayuzi@users.noreply.github.com>
2023-10-10 15:09:21 +0800

    Update BST.java
    添加测试代码

--- java/BinarySearchTree_05/BST.java
+++ java/BinarySearchTree_05/BST.java
@@ -47,7 +47,8 @@ public class BST<E extends Comparable> {
     //向以node为根的二分搜索树中插入元素e，递归算法
     //返回插入新节点后的二分搜索树
     public Node add(Node node, E e) {
-
+if(e instanceof  Node){
+}
       /* if (e.equals(node.e)){
            return;
        }else if (e.compareTo(node.e) < 0 && node.left == null){
